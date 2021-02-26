e9d089dc87df4dac85004dbe87184e303f6bd2bc
Nigless <nigless.gaming@gmail.com>
2021-02-26 20:00:02 +0300

    Added Greeting command

---
+++ src/commands/greeting.ts
@@ -0,0 +1,30 @@
+﻿import Config from '../config';
+import Message from '../message';
+import ICommand from './icommand';
+
+interface IConfig {
+	key: string;
+}
+
+export default class Greeting implements ICommand {
+	public key: string;
+	private config: IConfig;
+	private messages: { greet: string | string[] };
+
+	constructor(config: Config, messages: Config) {
+		this.config = config.create<IConfig>('hello', { key: 'hello' });
+		this.key = this.config.key;
+
+		this.messages = messages.create<{ greet: string | string[] }>('hello', {
+			greet: 'Hi!',
+		});
+	}
+
+	public run(): Message {
+		const greet = this.messages.greet;
+		if (Array.isArray(greet)) {
+			return new Message(greet[Math.floor(Math.random() * greet.length)]);
+		}
+		return new Message(greet);
+	}
+}
---
+++ src/commands/icommand.ts
@@ -0,0 +1,7 @@
+﻿import IMessage from '../message';
+
+export default interface ICommand<Options = void> {
+	key: string;
+
+	run(options: Options): IMessage;
+}
